b282e3903d32c71b2309fb5ba22a044e2431acbd
tamaki <tamaki@monosus.co.jp>
2017-11-24 18:23:17 +0900

    ex16 「１つのコンポーネントの中で複数の要素を配置する方法」　新規作成

---
+++ intro/ex16/Hello.js
@@ -0,0 +1,11 @@
+// ES6É½µ­
+class Evaluator extends React.Component{
+    render() {
+        return React.DOM.div(
+            null,
+            React.DOM.h2(null, 'First Element'),
+            React.DOM.h2(null, 'Second Element'),
+            React.DOM.h2(null, 'Third Element')
+        );
+    }
+}
---
+++ intro/ex16/app.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+    React.createElement(
+        Evaluator,
+        {
+            text: '5 / 4'
+        }
+    ),
+    document.getElementById('content')
+);
